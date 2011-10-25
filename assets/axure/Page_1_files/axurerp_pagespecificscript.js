8232bcd12107da85f4e204d1ca256ece2e1b89e9
Jonathan Hess <jonathan.hess@gmail.com>
2011-10-25 12:08:47 -0600

    Add axure assets

---
+++ assets/axure/Page_1_files/axurerp_pagespecificscript.js
@@ -0,0 +1,72 @@
+﻿
+var PageName = 'Page 1';
+var PageId = '45f18ac167e2427b8bf46f56337b06f4'
+var PageUrl = 'Page_1.html'
+document.title = 'Page 1';
+var PageNotes = 
+{
+"pageName":"Page 1",
+"showNotesNames":"False"}
+var $OnLoadVariable = '';
+
+var $CSUM;
+
+var hasQuery = false;
+var query = window.location.hash.substring(1);
+if (query.length > 0) hasQuery = true;
+var vars = query.split("&");
+for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if (pair[0].length > 0) eval("$" + pair[0] + " = decodeURIComponent(pair[1]);");
+} 
+
+if (hasQuery && $CSUM != 1) {
+alert('Prototype Warning: The variable values were too long to pass to this page.\nIf you are using IE, using Firefox will support more data.');
+}
+
+function GetQuerystring() {
+    return '#OnLoadVariable=' + encodeURIComponent($OnLoadVariable) + '&CSUM=1';
+}
+
+function PopulateVariables(value) {
+    var d = new Date();
+  value = value.replace(/\[\[OnLoadVariable\]\]/g, $OnLoadVariable);
+  value = value.replace(/\[\[PageName\]\]/g, PageName);
+  value = value.replace(/\[\[GenDay\]\]/g, '25');
+  value = value.replace(/\[\[GenMonth\]\]/g, '10');
+  value = value.replace(/\[\[GenMonthName\]\]/g, 'October');
+  value = value.replace(/\[\[GenDayOfWeek\]\]/g, 'Tuesday');
+  value = value.replace(/\[\[GenYear\]\]/g, '2011');
+  value = value.replace(/\[\[Day\]\]/g, d.getDate());
+  value = value.replace(/\[\[Month\]\]/g, d.getMonth() + 1);
+  value = value.replace(/\[\[MonthName\]\]/g, GetMonthString(d.getMonth()));
+  value = value.replace(/\[\[DayOfWeek\]\]/g, GetDayString(d.getDay()));
+  value = value.replace(/\[\[Year\]\]/g, d.getFullYear());
+  return value;
+}
+
+function OnLoad(e) {
+
+}
+
+var u0 = document.getElementById('u0');
+gv_vAlignTable['u0'] = 'top';
+var u1 = document.getElementById('u1');
+
+u1.style.cursor = 'pointer';
+if (bIE) u1.attachEvent("onclick", Clicku1);
+else u1.addEventListener("click", Clicku1, true);
+function Clicku1(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	self.location.href="Home.html" + GetQuerystring();
+
+}
+
+}
+
+if (window.OnLoad) OnLoad();
---
+++ assets/axure/data/sitemap.js
@@ -0,0 +1,12 @@
+﻿var sitemap = 
+{
+"rootNodes":[
+{
+"pageName":"Home",
+"type":"Wireframe",
+"url":"Home.html",
+"children":[
+{
+"pageName":"Page 1",
+"type":"Wireframe",
+"url":"Page_1.html"}]}]};
